15717aaaa3e2a1312ebd468f0f26c623b338461a
Jon Mikel Inza <jonmikeli@hotmail.com>
2020-03-14 00:06:17 +0100

    bash help

--- sources/move2cloud.sh
+++ sources/move2cloud.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
+function show_help {
+    echo "Usage: ${0} -s [subscriptionId] -g [resource group name] -n [vnet name]"
+    echo ""
+    echo "    -s: subscriptionId"
+    echo "    -g: resource group name"
+    echo "    -n: vnet name"
+	echo "    -t: tag (optional)"
+    echo "    -q: query (optional)"
+    echo "    -v: verbose (optional)"
+    exit 0
+}
+
 #function to get script parameter values
 get_parameter_values ()
 {
     echo "Azure-Move2VNet-Tool"
 
-	while getopts "s:t:p:l:e:r:f:" option;
+	while getopts "s:g:n:t:q:v:" option;
 	do	
 		case $option in
 			s) subscriptionId=$OPTARG;;
 			g) resourceGroupName=$OPTARG;;
-			v) vnet=$OPTARG;;
-			t) tags=$OPTARG;;
+			n) vnet=$OPTARG;;
+			t) tag=$OPTARG;;
             q) query=$OPTARG;;
             v) verbose=$OPTARG;;
+			*)
+				show_help
+				exit 0
+				;;
 		esac
 	done
 	shift $((OPTIND -1))	
@@ -47,10 +63,31 @@ if [ $? != 0 ]; then
 	echo "Using existing resource group..."
 fi
 
+#check if the target vnet exists
+az network vnet show --name $vnet -g $resourceGroupName 1> /dev/null
+
+if [ $? != 0 ]; then
+	echo "VNET with name" $vnet "could not be found in the resource group $resourceGroupName."
+	exit 1
+fi
+
+
 #Check for existing azure resources in the RG
-resources = az resources list --gresource-group $resourceGroupName --tag $tag --query $query
+if [-z "${resourceGroupName}" ]; then
+resources = az resources list --resource-group $resourceGroupName --tag $tag --query $query
+fi
+
+if []; then
+resources = az resources list --resource-group $resourceGroupName --tag $tag --query $query
+fi
+
+if []; then
+resources = az resources list --resource-group $resourceGroupName --tag $tag --query $query
+fi
+
 
 for r in $resources
 do
+
     echo "Processing resource $r ..."
 done
