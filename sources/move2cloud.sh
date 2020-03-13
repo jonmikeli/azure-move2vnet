95d033aee139da50d23898c8c7f4c82461e1ccbf
Jon Mikel Inza <jonmikeli@hotmail.com>
2020-03-13 14:01:25 +0100

    script updates

---
+++ sources/move2cloud.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#function to get script parameter values
+get_parameter_values ()
+{
+    echo "Azure-Move2VNet-Tool"
+
+	while getopts "s:t:p:l:e:r:f:" option;
+	do	
+		case $option in
+			s) subscriptionId=$OPTARG;;
+			r) resourceGroupName=$OPTARG;;
+			f) tags=$OPTARG;;
+            q) query=$OPTARG;;
+		esac
+	done
+	shift $((OPTIND -1))	
+
+	echo "Subscription id: $subscriptionId"
+	echo "Resource group name: $resourceGroupName"
+	echo "Tag: $tag"
+    echo "Query: $query"
+}
+
+#get script parameter values
+get_parameter_values "$@"
+
+#set the default subscription id
+az account set --subscription $subscriptionId
+
+#Check for existing RG
+az group show --name $resourceGroupName 1> /dev/null
+
+if [ $? != 0 ]; then
+	echo "Resource group with name" $resourceGroupName "could not be found."
+	set -e
+	(
+		set -x
+		az group create --name $resourceGroupName --location $resourceGroupLocation --subscription $subscriptionId --tags ${tags[@]} environment=$environment 1> /dev/null
+	)
+	else
+	echo "Using existing resource group..."
+fi
+
+#Check for existing azure resources in the RG
+az resources list --gresource-group $resourceGroupName --tag $tag --query $query 1> /dev/null
+
+if [ $? != 0 ]; then
+	echo "Resource group with name" $resourceGroupName "could not be found."
+	set -e
+	(
+		set -x
+		az group create --name $resourceGroupName --location $resourceGroupLocation --subscription $subscriptionId --tags ${tags[@]} environment=$environment 1> /dev/null
+	)
+	else
+	echo "Using existing resource group..."
+fi
